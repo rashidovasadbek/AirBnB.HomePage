ff2cc95f5c7d0a980b8e270e52e59ea947ba8ab5
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-13 22:49:55 +0500

    update files

---
+++ AirBnB.ServerApp/AirBnB.Api/Common/Mappers/LocationCategoryMapper.cs
@@ -0,0 +1,17 @@
+﻿using AirBnB.Api.Models.Dtos;
+using AirBnB.Domain.Entities;
+using AirBnB.Infrastructure.Extensions;
+using AirBnB.Infrastructure.Settings;
+using AutoMapper;
+using Microsoft.Extensions.Options;
+
+namespace AirBnB.Api.Common.Mappers;
+
+// public class LocationCategoryMapper : Profile
+// {
+//     // public LocationCategoryMapper()
+//     // {
+//     //     CreateMap<LocationCategory, LocationCategoryDto>()
+//     //         .ForMember(dest => dest, src => src.MapFrom(opt => opt.ImagePath.ToUrl()));
+//     // }
+// }
