f5055d9d1180472e0e9dea6c018e709000348cd3
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-14 17:28:44 +0500

    Added Configuration

---
+++ AirBnB.ServerApp/AirBnB.Persistence/EntityConfigurations/LocationCategoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using AirBnB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirBnB.Persistence.EntityConfigurations;
+
+public class LocationCategoryConfiguration : IEntityTypeConfiguration<LocationCategory>
+{
+    public void Configure(EntityTypeBuilder<LocationCategory> builder)
+    {
+        builder.Property(template => template.Name).IsRequired().HasMaxLength(256);
+        builder.Property(template => template.ImagePath).IsRequired().HasMaxLength(256);
+    }
+}
---
+++ AirBnB.ServerApp/AirBnB.Persistence/EntityConfigurations/LocationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AirBnB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirBnB.Persistence.EntityConfigurations;
+
+public class LocationConfiguration : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.Property(template => template.Name).IsRequired().HasMaxLength(256);
+        builder.Property(template => template.ImageUrl).IsRequired().HasMaxLength(256);
+        builder.Property(template => template.BuiltYear).IsRequired();
+        builder.Property(template => template.FeedBack).IsRequired();
+        builder.Property(template => template.PricePerNight).IsRequired();
+
+        builder.HasMany(location => location.LocationCategories)
+            .WithMany(locationCategory => locationCategory.Locations)
+            .UsingEntity<LocationRelation>();
+    }
+}
