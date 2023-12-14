9a2e23deeb002de30482bb124a25330bc78e1800
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-14 17:21:51 +0500

    Added LocationCategoriesController And LocationController

---
+++ AirBnB.ServerApp/AirBnB.Api/Controllers/LocationCategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using AirBnB.Api.Models.Dtos;
+using AirBnB.Application.Services;
+using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+using AirBnB.Infrastructure.Extensions;
+using AirBnB.Infrastructure.Settings;
+using AirBnB.Persistence.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AirBnB.Api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class LocationCategoriesController(ILocationCategoryService  locationCategoryService, IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public async ValueTask<IActionResult> GetLocationCategoryService(
+        [FromQuery] FilterPagination filterPagination,
+        [FromServices] IOptions<ApiSettings> apiSettings,
+        CancellationToken cancellationToken = default)
+    {
+        var querySpecification =
+            new QuerySpecification<LocationCategory>(filterPagination.PageSize, filterPagination.PageToken);
+        var result = await locationCategoryService.GetAsync(querySpecification, cancellationToken: cancellationToken);
+        var locationCategories = result.Select(locationCategory => new LocationCategoryDto
+        {
+            Id = locationCategory.Id,
+            Name = locationCategory.Name,
+            ImagePath = locationCategory.ImagePath.ToUrl(apiSettings.Value.ApiUrl),
+        });
+        
+        return locationCategories.Any() ? Ok(locationCategories) : BadRequest();
+    }
+}
---
+++ AirBnB.ServerApp/AirBnB.Api/Controllers/LocationController.cs
@@ -0,0 +1,30 @@
+﻿using AirBnB.Application.Services;
+using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirBnB.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LocationController : ControllerBase
+{
+    private readonly ILocationService _locationService;
+
+    public LocationController(ILocationService locationService)
+    {
+        _locationService = locationService;
+    }
+    [HttpGet]
+    public async ValueTask<IActionResult> GetLocations(
+        [FromQuery] FilterPagination paginationOptions,
+        [FromServices] ILocationService locationService,
+        CancellationToken cancellationToken = default)
+    {
+        var specificationA = new QuerySpecification<Location>(paginationOptions.PageSize, paginationOptions.PageToken);
+        var result = await locationService.GetByFilterAsync(specificationA, cancellationToken: cancellationToken);
+        return result.Any() ? Ok(result) : BadRequest();
+    }
+    
+    
+}
