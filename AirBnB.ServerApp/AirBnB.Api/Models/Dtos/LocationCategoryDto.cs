714ea699bf7bc0708667ee84cd605635fe4d558c
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-14 17:22:34 +0500

    Added Dto for LacationCategory and Lacation

---
+++ AirBnB.ServerApp/AirBnB.Api/Models/Dtos/LocationCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AirBnB.Api.Models.Dtos;
+
+public class LocationCategoryDto
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; } = default!;
+
+    public string ImagePath { get; set; } = default!;
+}
---
+++ AirBnB.ServerApp/AirBnB.Api/Models/Dtos/LocationDto.cs
@@ -0,0 +1,16 @@
+﻿namespace AirBnB.Api.Models.Dtos;
+
+public class LocationDto
+{
+    
+    public string ImageUrl { get; set; } = default!;
+    
+    public string Name { get; set; } = default!;
+    
+    public int BuiltYear { get; set; }
+    
+    public int PricePerNight { get; set; }
+    
+    public float FeedBack { get; set; }
+    
+}
