7107f6140509ef44742d9aec6c6507f3f6d9bfbe
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-14 17:29:03 +0500

    Add DbContext

---
+++ AirBnB.ServerApp/AirBnB.Persistence/DataContext/AirBnBdbContext.cs
@@ -0,0 +1,22 @@
+﻿using AirBnB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirBnB.Persistence.DataContext;
+
+public class AirBnBdbContext : DbContext
+{
+    public DbSet<Location> Locations => Set<Location>();
+    public DbSet<LocationCategory> LocationCategories => Set<LocationCategory>();
+
+    public DbSet<LocationRelation> LocationRelations => Set<LocationRelation>();
+
+    public AirBnBdbContext(DbContextOptions<AirBnBdbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AirBnBdbContext).Assembly);
+    }
+}
