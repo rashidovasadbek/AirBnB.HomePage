01ec390e7a93ab95836b1227ef8bbdc3557b1ec9
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-14 17:20:52 +0500

    Added  LocationMap files

---
+++ AirBnB.ServerApp/AirBnB.Api/Common/Mappers/LocationMap.cs
@@ -0,0 +1,13 @@
+﻿using AirBnB.Api.Models.Dtos;
+using AirBnB.Domain.Entities;
+using AutoMapper;
+
+namespace AirBnB.Api.Common.Mappers;
+
+public class LocationMap : Profile
+{
+    public LocationMap()
+    {
+        CreateMap<Location, LocationDto>();
+    }
+}
