3cf7be94bcbd4c7f14e26aa02dbcd7cd6a7431a8
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-14 17:30:53 +0500

    Added interface of service

---
+++ AirBnB.ServerApp/AirBnB.Application/Services/ILocationCategoryService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+
+namespace AirBnB.Application.Services;
+
+public interface ILocationCategoryService
+{
+    IQueryable<LocationCategory> Get(Expression<Func<LocationCategory, bool>>? predicate = default, bool asNoTracking = false);
+    
+    ValueTask<IList<LocationCategory>> GetAsync(
+        QuerySpecification<LocationCategory> querySpecification,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default
+    );
+    ValueTask<IList<LocationCategory>> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<LocationCategory?> GetByIdAsync(Guid locationId, bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+}
---
+++ AirBnB.ServerApp/AirBnB.Application/Services/ILocationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+
+namespace AirBnB.Application.Services;
+
+public interface ILocationService
+{
+    IQueryable<Location> Get(Expression<Func<Location, bool>>? predicate = default, bool asNoTracking = false);
+
+    ValueTask<IList<Location>> GetByFilterAsync(
+        QuerySpecification<Location> querySpecification,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+    ValueTask<Location> GetByIdAsync(Guid locationId,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Location> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+}
