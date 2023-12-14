7d84ec09e8355de1a6e6a7c117cfb712148e9338
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-14 17:41:49 +0500

    Added location Services

---
+++ AirBnB.ServerApp/AirBnB.Infrastructure/Services/LocationCategoryService.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+using AirBnB.Application.Services;
+using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+using AirBnB.Persistence.Repositories.Interface;
+
+namespace AirBnB.Infrastructure.Services;
+
+public class LocationCategoryService(ILocationCategoryRepository locationCategoryRepository) : ILocationCategoryService
+{
+    public IQueryable<LocationCategory> Get(Expression<Func<LocationCategory, bool>>? predicate = default, bool asNoTracking = false)
+    {
+        return locationCategoryRepository.Get(predicate, asNoTracking);
+    }
+
+    public async ValueTask<IList<LocationCategory>> GetAsync(QuerySpecification<LocationCategory> querySpecification, bool asNoTracking = false,
+        CancellationToken cancellationToken = default)
+    {
+        return await locationCategoryRepository.GetAsync(querySpecification, asNoTracking, cancellationToken);
+    }
+
+    public ValueTask<IList<LocationCategory>> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<LocationCategory?> GetByIdAsync(Guid locationId, bool asNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        return locationCategoryRepository.GetByIdAsync(locationId, asNoTracking, cancellationToken);
+    }
+}
---
+++ AirBnB.ServerApp/AirBnB.Infrastructure/Services/LocationService.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+using AirBnB.Application.Services;
+using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+using AirBnB.Persistence.Repositories.Interface;
+
+namespace AirBnB.Infrastructure.Services;
+
+public class LocationService(ILocationRepository locationRepository) : ILocationService
+{
+    
+    public IQueryable<Location> Get(Expression<Func<Location, bool>>? predicate = default, bool asNoTracking = false)
+    {
+        return locationRepository.Get(predicate, asNoTracking);
+    }
+    
+    public async ValueTask<IList<Location>> GetAsync(
+        QuerySpecification<Location> querySpecification,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return await locationRepository.GetAsync(querySpecification, asNoTracking, cancellationToken);
+    }
+
+    public async ValueTask<IList<Location>> GetByFilterAsync(
+        QuerySpecification<Location> querySpecification,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default)
+        => await GetAsync(querySpecification, asNoTracking, cancellationToken);
+    
+
+    public ValueTask<Location?> GetByIdAsync(Guid locationId, bool asNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        return locationRepository.GetByIdAsync(locationId, asNoTracking, cancellationToken);
+    }
+
+    public ValueTask<Location> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
