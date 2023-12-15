9506eec718b5147d2f39a9eb9fc5f8c68391c96c
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-15 21:40:22 +0500

    Added ts files

---
+++ vue-project/src/Infrastructures/query/FilterPagination.ts
@@ -0,0 +1,19 @@
+import type {IQueryConvertible} from "@/Infrastructures/query/IQueryConvertible";
+
+export class FilterPagination implements IQueryConvertible{
+    pageSize: number = 10;
+    pageToken: number = 1;
+
+    constructor(pageSize: number, pageToken: number) {
+        this.pageSize = pageSize;
+        this.pageToken = pageToken;
+    }
+
+    public toQueryParams() : URLSearchParams {
+        const params = new URLSearchParams();
+        params.append("pageSize", this.pageSize.toString());
+        params.append("pageToken", this.pageToken.toString())
+
+        return params;
+    }
+}
