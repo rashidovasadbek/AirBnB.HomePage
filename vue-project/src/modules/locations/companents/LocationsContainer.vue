d6196f11fbfeec45f80a9a3d8161f1f00e796688
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-15 21:40:51 +0500

    Added vue fiels

---
+++ vue-project/src/modules/locations/companents/LocationsContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <!--Content-->
+  <article class="mt-[20px] px[24px] px-[80px]">
+
+    <div class="flex flex-col items-center justify-center">
+
+      <!-- Locations tap-->
+      <locations-tap />
+
+      <!--LocationsGrid-->
+      <locations-grid/>
+
+
+    </div>
+
+  </article>
+</template>
+<script setup lang="ts">
+import LocationsTap from "@/modules/locations/companents/LocationsTap.vue";
+import LocationsGrid from "@/modules/locations/companents/LocationsGrid.vue";
+</script>
---
+++ vue-project/src/modules/locations/companents/LocationsGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mt-[160px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-y-10 gap-x-4 place-items-center">
+
+    <location-card v-for="location in locations" :location="location"/>
+
+  </div>
+</template>
+<script setup lang="ts">
+
+import LocationCard from "@/modules/locations/companents/LocationCard.vue";
+import {AirBnBApiClient} from "@/Infrastructures/AirBnBApiClient/brokers/AirBnBApiClient";
+import {onBeforeMount, ref} from "vue";
+import {LocationFilter} from "@/modules/locations/models/LocationFilter";
+
+const airBnbApiClient = new AirBnBApiClient();
+
+const locations = ref<Location[]>([])
+
+onBeforeMount(async () => {
+    const filter = new LocationFilter("Castle", 50, 1)
+    const locationsResponse = await airBnbApiClient.locations.getAsync(filter);
+
+    locations.value = locationsResponse.response;
+
+    console.log('response', locationsResponse)
+});
+</script>
