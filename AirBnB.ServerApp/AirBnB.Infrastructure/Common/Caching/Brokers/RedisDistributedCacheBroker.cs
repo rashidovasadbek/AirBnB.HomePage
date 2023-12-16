28cc83f0ce16d5209d2c519e6f97a4f65913374a
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-16 22:11:32 +0500

    update RedisDistributedCacheBroker

--- AirBnB.ServerApp/AirBnB.Infrastructure/Common/Caching/Brokers/RedisDistributedCacheBroker.cs
+++ AirBnB.ServerApp/AirBnB.Infrastructure/Common/Caching/Brokers/RedisDistributedCacheBroker.cs
@@ -16,10 +16,16 @@ public class RedisDistributedCacheBroker(IOptions<CacheSettings> cacheSettings, 
         AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(1200),//cacheSettings.Value.AbsoluteExpirationIsSeconds),
         SlidingExpiration = TimeSpan.FromSeconds(60),//cacheSettings.Value.SlidingExpirationInSeconds)
     };
+
+    private readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+    {
+        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+    };
+    
     public async ValueTask<T> GetASync<T>(string key)
     {
         var value = await distributedCache.GetAsync(key);
-        return value is not null ? JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(value)) : default;
+        return value is not null ? JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(value), JsonSettings) : default;
     }
 
     public ValueTask<bool> TryGetAsync<T>(string key, out T? value)
@@ -28,7 +34,7 @@ public class RedisDistributedCacheBroker(IOptions<CacheSettings> cacheSettings, 
 
         if (foundEntry is not null)
         {
-            value = JsonConvert.DeserializeObject<T>(foundEntry);
+            value = JsonConvert.DeserializeObject<T>(foundEntry, JsonSettings);
             return ValueTask.FromResult(true);
         }
 
@@ -39,7 +45,7 @@ public class RedisDistributedCacheBroker(IOptions<CacheSettings> cacheSettings, 
     public async ValueTask<T> GetOrSetAsync<T>(string key, Func<Task<T>> valueFactory, CacheEntryOptions? cacheEntryOptions = default)
     {
         var cachedValue = await distributedCache.GetStringAsync(key);
-        if (cachedValue is not null) return JsonConvert.DeserializeObject<T>(cachedValue);
+        if (cachedValue is not null) return JsonConvert.DeserializeObject<T>(cachedValue, JsonSettings);
 
         var value = await valueFactory();
         await SetAsync(key, await valueFactory(), cacheEntryOptions);
@@ -49,7 +55,7 @@ public class RedisDistributedCacheBroker(IOptions<CacheSettings> cacheSettings, 
 
     public async ValueTask SetAsync<T>(string key, T value, CacheEntryOptions? cacheEntryOptions = default)
     {
-        await distributedCache.SetStringAsync(key, JsonConvert.SerializeObject(value),
+        await distributedCache.SetStringAsync(key, JsonConvert.SerializeObject(value, JsonSettings),
             GetCacheEntryOptions(cacheEntryOptions));
     }
 
