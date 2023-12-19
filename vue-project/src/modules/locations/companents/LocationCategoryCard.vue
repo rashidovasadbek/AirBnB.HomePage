995687d810e42a7c01b4cce289ef9965211bd9a9
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-19 21:49:55 +0500

    update LocationCategoryCard

--- vue-project/src/modules/locations/companents/LocationCategoryCard.vue
+++ vue-project/src/modules/locations/companents/LocationCategoryCard.vue
@@ -1,44 +1,64 @@
 <template>
 
-  <button type="button" @click="filter">
-    <div class="h-[80px] select-common group min-w-[50px] flex flex-col justify-center items-center">
-      <img class="h-[24px] w-[24px] mb-2 select-logo-img select-transition"
+  <!--  <div v-if="isSelected">Selected</div>-->
+
+  <button type="button" @click="onCategorySelected" class="select-none">
+    <span class="h-[80px] select-common group min-w-[50px] flex flex-col justify-center items-center">
+      <img class="h-[24px] w-[24px] mb-2  select-transition"
            :src="locationCategory.imagePath"
-           alt="Location selection icon">
-      <h5 class="text-xs font-medium select-text select-transition whitespace-nowrap">{{locationCategory.name}}</h5>
-      <div class="mt-[10px] w-4/5 h-[2px] select-item select-transition"></div>
-    </div>
+           alt="Location selection icon"
+           :class="isSelected ? 'selected-opacity': 'opacity-40 select-opacity'">
+      <span
+          class="text-xs font-medium whitespace-nowrap select-none"
+          :class="isSelected ? 'selected-opacity': 'opacity-40 select-opacity'"
+      >{{ locationCategory.name }}</span>
+      <span class="mt-[10px] w-4/5 h-[2px] bg-black"
+            :class="isSelected ? 'selected-opacity': 'opacity-0 select-opacity'"
+      ></span>
+    </span>
   </button>
 
 </template>
 
 <script setup lang="ts">
 
-import {defineProps, onBeforeMount} from "vue";
-import type { LocationCategory } from "../models/locationCategory";
-import locationCategories from "@/modules/locations/companents/LocationCategories.vue";
-import {AirBnBApiClient} from "@/Infrastructures/AirBnBApiClient/brokers/AirBnBApiClient";
+import {defineProps, defineEmits, computed} from "vue";
+import type {LocationCategory} from "../models/locationCategory";
 
 const props = defineProps({
-    locationCategory: {
-      type:  Object as () => LocationCategory,
-      required: true
-    }
+  selectedCategoryId: {
+    type: String,
+    required: true
+  },
+  locationCategory: {
+    type: Object as () => LocationCategory,
+    required: true
+  }
 });
 
-var airBnbApiClient = new AirBnBApiClient();
-function filter(event: any) {
-  alert(
-      /*onBeforeMount(async () =>
-      {
-        const locationResponse = await airBnbApiClient.locationCategories.getAsync();
-        locationCategories.value = locationResponse.response!;
+const emit = defineEmits<{
+  'onCategorySelected': [id: string]
+}>();
 
-        console.log("test", locationCategories.value)
-    })*/
-  )
-  // `event` is the native DOM event
-  if (event) {
-  }
+
+const onCategorySelected = () => {
+  emit('onCategorySelected', props.locationCategory.id);
 }
+
+const isSelected = computed(() => props.locationCategory.id === props.selectedCategoryId);
+
+// function filter(event: any) {
+//   alert(
+//       onBeforeMount(async () => {
+//         const locationResponse = await airBnbApiClient.locationCategories.getAsync();
+//         locationCategories.value = locationResponse.response!;
+//
+//         console.log("test", locationCategories.value)
+//       })
+//   )
+//   // `event` is the native DOM event
+//   if (event) {
+//
+//   }
+// }
 </script>
