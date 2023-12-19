9c84e51e493fcce90646c2b1905df38b826fe0d9
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-19 21:50:22 +0500

    Update LocationsContainer

--- vue-project/src/modules/locations/companents/LocationsContainer.vue
+++ vue-project/src/modules/locations/companents/LocationsContainer.vue
@@ -5,11 +5,10 @@
     <div class="flex flex-col items-center justify-center">
 
       <!-- Locations tap-->
-      <locations-tap />
+      <locations-tap @onCategorySelected="onCategorySelected" :selectedCategoryId="selectedCategoryId"/>
 
       <!--LocationsGrid-->
       <locations-grid/>
-
 
     </div>
 
@@ -18,4 +17,12 @@
 <script setup lang="ts">
 import LocationsTap from "@/modules/locations/companents/LocationsTap.vue";
 import LocationsGrid from "@/modules/locations/companents/LocationsGrid.vue";
+import {ref} from "vue";
+
+const selectedCategoryId = ref<string>("");
+
+const onCategorySelected = (id: string) => {
+  selectedCategoryId.value = id;
+}
+
 </script>
