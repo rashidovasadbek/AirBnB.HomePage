622cd560ed4a4489fbc3615bbce577e08aee3e8d
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-19 21:49:34 +0500

    Update LocationCategories

--- vue-project/src/modules/locations/companents/LocationCategories.vue
+++ vue-project/src/modules/locations/companents/LocationCategories.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex gap-6 md:gap-12 overflow-x-scroll no-scrollbar">
-    <location-category-card v-for ="location in locationCategories" :locationCategory ="location"/>
-
+    <location-category-card v-for="location in locationCategories" @onCategorySelected="onCategorySelected"
+                            :locationCategory="location" :selectedCategoryId="selectedCategoryId"/>
   </div>
 </template>
 
 <script lang="ts" setup>
 
 import {AirBnBApiClient} from "@/Infrastructures/AirBnBApiClient/brokers/AirBnBApiClient";
-import {onBeforeMount, ref} from "vue";
+import {defineEmits, defineProps, onBeforeMount, ref} from "vue";
 import LocationCategoryCard from "@/modules/locations/companents/LocationCategoryCard.vue";
 import {LocationCategory} from "@/modules/locations/models/locationCategory";
 
-const  airBnbApiClient = new AirBnBApiClient();
+const airBnbApiClient = new AirBnBApiClient();
 const locationCategories = ref<LocationCategory[]>([]);
 
 onBeforeMount(async () => {
@@ -22,4 +22,20 @@ onBeforeMount(async () => {
   console.log("test", locationCategories.value)
 
 });
+
+const props = defineProps({
+  selectedCategoryId: {
+    type: String,
+    required: true
+  }
+});
+
+const emit = defineEmits<{
+  'onCategorySelected': [id: string]
+}>();
+
+const onCategorySelected = (id: string) => {
+  emit('onCategorySelected', id);
+}
+
 </script>
