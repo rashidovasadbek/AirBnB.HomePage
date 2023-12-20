149dbe51ef391cb5839e6490467b9c593e210450
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-20 22:39:44 +0500

    update location and LocationCategory ts

--- vue-project/src/modules/locations/models/location.ts
+++
@@ -1,7 +0,0 @@
-
-export class Location {
-    public imageUrl!: string;
-    public name!: string;
-    public buildYear!: number;
-    public pricePerNight!: number;
-}
--- vue-project/src/modules/locations/models/locationCategory.ts
+++
@@ -1,7 +0,0 @@
-import type {Guid} from "guid-typescript";
-
-export class LocationCategory {
-    public id!: string;
-    public name!: string;
-    public imagePath!: string;
-}
