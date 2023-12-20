d9320edecdf8b58f3d08d1c46bf7fc6d481d2533
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-20 22:43:27 +0500

    Update LocationFIlter

--- vue-project/src/modules/locations/models/LocationFilter.ts
+++ vue-project/src/modules/locations/models/LocationFilter.ts
@@ -1,17 +1,21 @@
 import {FilterPagination} from "@/Infrastructures/query/FilterPagination";
+import type {Guid} from "guid-typescript";
 
 export class LocationFilter extends FilterPagination {
-    public category: string;
 
-    constructor(category: string, pageSize: number, pageToken: number) {
+    public categoryId: string | null;
+
+    constructor(categoryId: string | null, pageSize: number, pageToken: number) {
         super(pageSize, pageToken);
 
-        this.category = category;
+        this.categoryId = categoryId;
     }
 
     public override toQueryParams(): URLSearchParams {
         const params = super.toQueryParams()
-        params.append("category", this.category);
+        if(this.categoryId != null){
+            params.append("categoryId", this.categoryId);
+        }
 
         return params;
     }
